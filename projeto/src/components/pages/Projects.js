25b864ba7336ef347dc7022910cd7dbc5843b341
JovemDevv <anacarolinesan18@gmail.com>
2023-07-02 23:40:03 -0300

    banco de dados

--- projeto/src/components/pages/Projects.js
+++ projeto/src/components/pages/Projects.js
@@ -1,18 +1,38 @@
 import { useLocation } from 'react-router-dom'
 
+import { useEffect, useState } from 'react'
+
 import Message from '../layout/Message'
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../project/ProjectCard'
+
 
 import styles from './Projects.module.css'
 
 function Projects() {
+    const [projects, setProjects] = useState([])
 
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(resp => resp.json())
+        .then((data) => {
+            console.log(data)
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
 
     return (
         <div className={styles.project_container}>
@@ -22,7 +42,8 @@ function Projects() {
             </div>
             {message && <Message type="success" msg={message} />}
             <Container costomClass="start">
-                <p>Projetos...</p>
+                {projects.length > 0 &&
+                }
             </Container>
         </div>
     )
---
+++ projeto/src/components/project/ProjectCard.js
@@ -0,0 +1,10 @@
+import styles from './ProjectCard.module.css'
+
+import {BsPencil, BsFillTrashFill} from 'react-icons/bs'
+
+function ProjectCard({ id, name, budget, category, handleRemove }) {
+    return (
+        <p>projeto</p>
+    )
+}
+export default ProjectCard
